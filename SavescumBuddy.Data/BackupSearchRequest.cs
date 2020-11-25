07a85291f281da26f4b82b50723fec0f88541084
bokuwazheng <yevgeny.kolomensky@gmail.com>
2020-11-25 17:59:58 +0300

    settings ui changes

--- SavescumBuddy.Data/BackupSearchRequest.cs
+++ SavescumBuddy.Data/BackupSearchRequest.cs
@@ -10,6 +10,6 @@
         public bool? IsLiked { get; set; }
         public bool? IsAutobackup { get; set; }
         public bool? IsInGoogleDrive { get; set; }
-        public int GameId { get; set; }
+        public int? GameId { get; set; }
     }
 }
